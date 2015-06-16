5aa089c1523b3f8811540d1221dccf518329d5cf
EugenePWhatley <epwhatley@gmail.com>
2015-06-15 23:16:36 -0500

    Update Account.java

--- src/test/java/com/thoughtworks/accountbalance/Account.java
+++ src/test/java/com/thoughtworks/accountbalance/Account.java
@@ -4,20 +4,24 @@ package com.thoughtworks.accountbalance;
  * Created by Eugene on 6/8/2015.
  */
 public class Account {
-    private float balance;
+    private int balance;
 
-    public Account(float balance) {
+    public Account(int balance) {
         this.balance = balance;
     }
 
-    public float deposit(float amount) {
+    public int deposit(int amount) {
         balance+=amount;
         return balance;
     }
 
-    public float withdraw(float amount) {
+    public int withdraw(int amount) {
         if(balance > amount)
             balance-=amount;
+        return balance;
+    }
+    
+    public int balance(){
         return balance;
     }
 }
